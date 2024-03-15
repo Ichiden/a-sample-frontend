105ad1b64fd1e1689ac73e699e74dac73ecabfed
Ichiden <111832766+Ichiden@users.noreply.github.com>
2024-03-15 09:24:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query'
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:5000"
+axios.defaults.baseURL = "https://a-sample-backend.onrender.com"
 
 
 
